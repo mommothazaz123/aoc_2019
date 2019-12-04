7007438c230baab634ec240fc9a09281ac9a45ed
Andrew Zhu <me@andrew-zhu.com>
2019-12-04 00:19:29 -0800

    add day 4

---
+++ 4/2.py
@@ -0,0 +1,26 @@
+# in: 152085-670283
+
+
+def run():
+    small, big = map(int, input().split('-'))
+
+    valid = 0
+
+    for i in range(small, big + 1):
+        i_ = str(i)
+        if not len(i_) == 6:
+            continue
+
+        if not any(char * 2 in i_ and char * 3 not in i_ for char in i_):
+            continue
+
+        if not all(int(i_[j]) >= int(i_[j - 1]) for j in range(1, 6)):
+            continue
+
+        valid += 1
+
+    print(valid)
+
+
+if __name__ == '__main__':
+    run()
